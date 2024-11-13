4fad128e817bc75804a4d182cb6e6e8aa4457aa7
rpjc <matvey5i@ukr.net>
2024-11-13 14:03:00 +0200

    Hotfix: implement 3 term cosine series

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,7 +1,9 @@
 #include "FuncA.h"
 #include <cmath>
 
-double FuncA::calculate() {
-//placeholder
-    return 0;
+double FuncA::calculate(double x, int n) {
+// Calculate first 3 terms of cosine Taylor series
+    double x2 = x * x;
+    double x4 = x2 * x2;
+    return 1.0 - x2/2.0 + x4/24.0;
 }
