b9b58feb477ea4118299cbe2db7398449ea4ccb1
rpjc <matvey5i@ukr.net>
2024-11-26 22:00:30 +0200

    implemented basic HTTP server

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include "FuncA.h"
 
+int CreateHTTPserver();
+
 int main() {
     FuncA calc;
-    double x = 1.0; // Example value for x
-    int n = 10; // Example value for n
-    std::cout << "FuncA result: " << calc.calculate(x, n) << std::endl;
+    // double x = 1.0; // Example value for x
+    // int n = 10; // Example value for n
+    // std::cout << "FuncA result: " << calc.calculate(x, n) << std::endl;
+
+
+    CreateHTTPserver();
+
+
     return 0;
 }
