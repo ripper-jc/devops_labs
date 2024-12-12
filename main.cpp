f80e9e8dc2c32aa579481b208df8162ba2a4f871
Matvii_rpjc <matvey5i@ukr.net>
2024-12-12 23:10:47 +0200

    add signal handlers

--- main.cpp
+++ main.cpp
@@ -1,9 +1,48 @@
 #include <iostream>
+
+#include <sys/wait.h>
 #include "FuncA.h"
 
 int CreateHTTPserver();
 
+void sigchldHandler(int s) {
+    printf("SIGCHLD received\n");
+    pid_t pid;
+    int status;
+
+    while((pid = waitpid(-1, &status, WNOHANG)) > 0);
+    {
+        if (WIFEXITED(status)) {
+            printf("Child %d terminated with status: %d\n", pid, WEXITSTATUS(status));
+        } else {
+            printf("Child %d terminated abnormally\n", pid);
+        }
+    }
+}
+
+void siginHandler(int s) {
+    printf("Caught signal %d\n", s);
+    pid_t pid;
+    int status;
+    while((pid = waitpid(-1, &status, 0)) > 0);
+    {
+        if (WIFEXITED(status)) {
+            printf("Child %d terminated with status: %d\n", pid, WEXITSTATUS(status));
+        } else {
+            printf("Child %d terminated abnormally\n", pid);
+        }
+        if (pid == -1) {
+            printf("No more child processes\n");
+        }
+    }
+}
+
+
 int main() {
+    
+    signal(SIGCHLD, sigchldHandler);
+    signal(SIGINT, siginHandler);
+
     FuncA calc;
     // double x = 1.0; // Example value for x
     // int n = 10; // Example value for n
---
+++ terminalCommandRunner.sh
@@ -0,0 +1,2 @@
+gnome-terminal -- bash -c "curl -i -X GET 127.0.0.1/compute; exec $SHELL"
+
